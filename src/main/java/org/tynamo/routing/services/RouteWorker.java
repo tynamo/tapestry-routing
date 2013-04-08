45f8ce547933b5112e8f9bd77054c54e911da89a
Alejandro Scandroli <ascandroli@codehaus.org>
2013-04-08 21:52:38 +0000

    introducing the @Route annotation

--- src/main/java/org/tynamo/routing/services/RouteWorker.java
+++ src/main/java/org/tynamo/routing/services/RouteWorker.java
@@ -5,8 +5,8 @@ import org.apache.tapestry5.plastic.PlasticClass;
 import org.apache.tapestry5.services.ComponentClassResolver;
 import org.apache.tapestry5.services.transform.ComponentClassTransformWorker2;
 import org.apache.tapestry5.services.transform.TransformationSupport;
-import org.tynamo.routing.Route;
 import org.tynamo.routing.annotations.At;
+import org.tynamo.routing.annotations.Route;
 
 public class RouteWorker implements ComponentClassTransformWorker2 {
 
@@ -22,14 +22,25 @@ public class RouteWorker implements ComponentClassTransformWorker2 {
 
 	@Override
 	public void transform(PlasticClass plasticClass, TransformationSupport support, MutableComponentModel model) {
+
+		String pathExpression = null;
+		String[] order;
+
 		if (plasticClass.hasAnnotation(At.class)) {
 			At ann = plasticClass.getAnnotation(At.class);
-			String pathExpression = ann.value();
+			pathExpression = ann.value();
+			order = ann.order();
+		} else if (plasticClass.hasAnnotation(Route.class)) {
+			Route ann = plasticClass.getAnnotation(Route.class);
+			pathExpression = ann.value();
+			order = ann.order();
+		}
 
+		if (pathExpression != null) {
 			String pageName = componentClassResolver.resolvePageClassNameToPageName(plasticClass.getClassName());
 			String canonicalized = componentClassResolver.canonicalizePageName(pageName);
 
-			Route route = routeFactory.create(pathExpression, canonicalized);
+			org.tynamo.routing.Route route = routeFactory.create(pathExpression, canonicalized);
 
 			annotatedPagesManager.add(route);
 		}
