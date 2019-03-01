55b1ae7c170b1d5f6f55dc1cdcf429d8333c4586
pico.dev <pico.dev@gmail.com>
2019-03-01 20:46:48 +0100

    Fix tests
    Fixes #4

--- src/test/java/org/tynamo/routing/pages/subpackage/SubPageFirst.java
+++ src/test/java/org/tynamo/routing/pages/subpackage/SubPageFirst.java
@@ -3,7 +3,7 @@ package org.tynamo.routing.pages.subpackage;
 
 import org.tynamo.routing.annotations.At;
 
-@At(value = "/subpackage/inventedpath", order = "before:subpackage/SubPage")
+@At(value = "/subpackage/inventedpath", order = "before:subpackage/Sub,before:subpackage/SubPage")
 public class SubPageFirst {
 	protected void onActivate() {}
 }
